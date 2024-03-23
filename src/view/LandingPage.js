76668857ee4c47ba11f5db3de608b24bf7bdca0a
lena <107754913+leonorlyn@users.noreply.github.com>
2024-03-22 23:20:46 -0700

    feat:test

--- src/view/LandingPage.js
+++ src/view/LandingPage.js
@@ -5,6 +5,7 @@ import ProductHeroLayout from '../elements/ProductHeroLayout';
 import TypewriterEffect from '../function/TypewriterEffect';
 import NavBar from '../elements/NavBar';
 import WhyPanboo from '../elements/WhyPanboo';
+import Story from '../elements/Story';
 
 const backgroundImage =
   'https://images.unsplash.com/photo-1586581277029-5769487f3881?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
@@ -63,6 +64,7 @@ export default function LandingPage() {
       </Button>
     </ProductHeroLayout>
     <WhyPanboo/>
+    <Story />
     </React.Fragment>
   );
 }
